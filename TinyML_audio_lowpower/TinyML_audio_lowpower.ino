2e6db33fdd8471ea998e4f23496c3fe898445a25
R. D. Poor <rdpoor@gmail.com>
2022-01-29 14:48:30 -0800

    Clean up TinyML_audio_lowpower.ino

--- TinyML_audio_lowpower/TinyML_audio_lowpower.ino
+++ TinyML_audio_lowpower/TinyML_audio_lowpower.ino
@@ -1,86 +1,128 @@
-/*
- * Copyright (c) 2021 Syntiant Corp.  All rights reserved.
- * Contact at http://www.syntiant.com
+/**
+ * MIT License
  *
- * This software is available to you under a choice of one of two licenses.
- * You may choose to be licensed under the terms of the GNU General Public
- * License (GPL) Version 2, available from the file LICENSE in the main
- * directory of this source tree, or the OpenIB.org BSD license below.  Any
- * code involving Linux software will require selection of the GNU General
- * Public License (GPL) Version 2.
+ * Copyright (c) 2022 R. Dunbar Poor
  *
- * OPENIB.ORG BSD LICENSE
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
-#include <NDP.h>
-#include <NDP_utils.h>
-#include <Arduino.h>
-#include "TinyML_init.h"
+#include "NDP_init.h"
 #include "NDP_loadModel.h"
 #include "SAMD21_init.h"
-#include "NDP_init.h"
 #include "SAMD21_lowpower.h"
+#include "TinyML_init.h"
+#include <Arduino.h>
+#include <NDP.h>
+#include <NDP_utils.h>
 
-int match = 0; // This variable indicates which class has matched
+typedef enum {
+  MATCH_NONE = 0,
+  MATCH_PLAY_MUSIC,
+  MATCH_STOP_MUSIC,
+} classifier_match_t;
 
-static void wakeup_from_sleep(void) { //This routine enables functions disabled in prep_for_sleep() and polls NDP
-  systick_enable();
-  usb_serial_enable();
-  adc_enable();
-  match = NDP.poll();
-}
+typedef enum {
+  NDP_MICROPHONE = 0,
+  NDP_SENSOR = 1,
+} ndp_source_t;
 
-void NDP_INT_service(){
-  switch (match){
-    case 0:
-      break;
-    case 1:
-      digitalWrite(LED_BLUE, HIGH);                       // Setting up Blue LED
-      Serial.println("Classifier 0 detected");            // printing on the native console
-      delay(1000);                                        // Delay to keep the LED on for 1 second. Reduce it to reduce total power consumption
-      digitalWrite(LED_BLUE, LOW);                        // Turning off the Blue LED
-      break;
-    case 2:
-      digitalWrite(LED_GREEN, HIGH);
-      Serial.println("Classifier 1 detected");
-      delay(1000);
-      digitalWrite(LED_GREEN, LOW);
-      break;
-    default:
-      break;
+// Written at interrupt level (ndp_isr), read in service_ndp()
+static volatile classifier_match_t s_match;
+
+/**
+ * @brief Called at interrupt level when the NDP asserts an interrupt.
+ */
+static void ndp_isr(void) { s_match = (classifier_match_t)NDP.poll(); }
+
+void service_ndp() {
+  switch (s_match) {
+  case MATCH_NONE:
+    break;
+
+  case MATCH_PLAY_MUSIC:
+    // 'Alexa, play music'
+    digitalWrite(LED_BLUE, HIGH);
+    Serial.println("Classifier 0 detected");
+    delay(1000);
+    digitalWrite(LED_BLUE, LOW);
+    break;
+
+  case MATCH_STOP_MUSIC:
+    // 'Alexa, stop music'
+    digitalWrite(LED_GREEN, HIGH);
+    Serial.println("Classifier 1 detected");
+    delay(1000);
+    digitalWrite(LED_GREEN, LOW);
+    break;
+
+  default:
+    break;
   }
 }
 
+/**
+ * @brief One-time setup, called upon reboot.
+ */
 void setup(void) {
-  SAMD21_init(0);                                          // Setting up SAMD21 
-  NDP_init("google10.bin",0);                             // Setting up NDP "google10.bin" is factory set model for Alexa play music, "ei_model.bin" is Edge Impulse model, second parameter is 0 for Microphone and 1 for Sensor
-  attachInterrupt(NDP_INT, wakeup_from_sleep, HIGH);      // Enabling interrupt based on NDP_INT signal and executing wakeup_from_sleep() routine
-  NVMCTRL->CTRLB.bit.SLEEPPRM = NVMCTRL_CTRLB_SLEEPPRM_DISABLED_Val;// Prevent the internal flash memory from powering down in sleep mode
-  SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;                      // Select STANDBY for sleep mode
+  // Initialize the SAMD21 host processor
+  SAMD21_init(0);
+  // Install the model for "Alexa, play music" and "Alexa, stop music".  The
+  // model was created using Edge Impulse.
+  NDP_init("google10.bin", NDP_MICROPHONE);
+  // The NDP101 will wake the SAMD21 upon detection
+  attachInterrupt(NDP_INT, ndp_isr, HIGH);
+  // Prevent the internal flash memory from powering down in sleep mode
+  NVMCTRL->CTRLB.bit.SLEEPPRM = NVMCTRL_CTRLB_SLEEPPRM_DISABLED_Val;
+  // Select STANDBY for sleep mode
+  SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;
 }
 
+/**
+ * @brief main loop.
+ *
+ * This loop immediately puts the processor into low-power standby mode, then
+ * waits for an interrupt from the NDP.  Upon receiving the interrupt, the
+ * processor wakes, services the NDP request and goes back to sleep.
+ *
+ * NOTE: To conserve power, the SAMD21 disables the USB port when it sleeps
+ * (which is most of the time).  When the USB port is disabled, any attempt to
+ * upload a sketch will fail.
+ *
+ * In order to upload a sketch, double-click the reset button.  This will put
+ * the SAMD21 into bootloader mode with the USB port enabled, allowing you to
+ * upload a sketch.
+ */
 void loop() {
-  //WARNING: The USB connection is disconnected. Double click on Reset pin to put the board in Bootloader mode for uploading next time.
-  prep_for_sleep();                                       // Preparing for sleep
-  __DSB();                                                // Ensure remaining memory accesses are complete
-  __WFI();                                                // Enter sleep mode and Wait For Interrupt (WFI), When NDP_INT goes high, wakeup_from_sleep() runs through attachInterrupt
-  NDP_INT_service();                                      // match variable is already set in the wakeup_from_sleep() routine. LEDs and GPIOs are toggled here.
+  // Put various peripheral modules into low power mode before entering standby.
+  adc_disable();        // See SAMD21_lowpower.h
+  usb_serial_disable(); // See note above about USB communications
+  systick_disable();
+  // Complete any memory operations and enter standby mode until an interrupt
+  // is recieved from the NDP101
+  __DSB();
+  __WFI();
+  // Arrive here after waking and having called ndp_isr().  Re-enable various
+  // peripheral modules before servicing the NDP classifier data.
+  systick_enable();
+  usb_serial_enable();
+  adc_enable();
+  // process the classifier data from the NDP
+  service_ndp();
+  // loop (and return immediately to standby mode)
 }
